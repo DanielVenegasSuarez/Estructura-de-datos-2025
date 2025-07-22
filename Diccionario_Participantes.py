6f1a7cacf638c56ac44f0ecb4c2b5c8d9cab33c3
LuisBMos <lbustosm@unal.edu.co>
2025-07-22 15:33:10 -0500

    Participantes
    Modificación de Miguel

--- Diccionario_Participantes.py
+++ Diccionario_Participantes.py
@@ -14,12 +14,40 @@ def registrarParticipantes(nombre, pais, deporte, edad, genero):
         }
     escribirParticipantes(parti)
 
-def eliminarDeBase(nombre):
+def eliminarDeBase(nombre, pais):
     base = leerParticiapantes()
     if nombre in base:
-        base[nombre].pop()
-        escribirParticipantes(base)
+        if base[nombre]["Pais"] == pais:
+            del base[nombre]
+            escribirParticipantes(base)
+            return True
+        else:
+            return False  # El nombre existe pero no corresponde al país
+    return False  # El nombre no existe
+
+
+def participanteExiste(nombre, pais):
+    participantes = leerParticiapantes()
+    if nombre in participantes and participantes[nombre]["Pais"] == pais:
         return True
+    return False
+
+        
+def mostrar_participante(nombre_participante, pais):
+    participantes = leerParticiapantes()
+    if nombre_participante in participantes:
+        datos = participantes[nombre_participante]
+        if datos["Pais"] == pais:
+            info = [
+                nombre_participante,
+                datos["Pais"],
+                datos["Genero"],
+                datos["Edad"],
+                datos["Deporte"]
+            ]
+            return info
+        else:
+            return "El participante existe, pero no pertenece al país especificado."
     else:
-        return False
+        return "El participante no existe en la base de datos."
 
